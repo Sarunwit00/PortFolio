3caf101e5c353900d042bd07cdccfccf127e6336
Sarunwit00 <a.sarunwit@gmail.com>
2025-09-25 01:10:31 +0700

    Save3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from './components/hero/Hero'
 
 export default function App() {
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello world!
-    </h1>
+    <main className=' bg-[#0d182e]'>
+      <Hero />
+    </main>
   )
 }
