97d481791e79d2d9450e1585352efa70d87c3dbb
Sarunwit00 <a.sarunwit@gmail.com>
2025-09-25 19:24:35 +0700

    first-commid

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,51 +1,51 @@
-import React from 'react'
-import Navbar from '../navbar/Navbar'
-import CV from './CV.png'
-import img from '../../assets/img.png'
+// import React from 'react'
+// import Navbar from '../navbar/Navbar'
+// import CV from './CV.png'
+// import img from '../../assets/img.png'
 
-export default function Hero() {
-  return (
-    <div className='relative overflow-hidden min-h-[550px] sm:min-h-[660px] flex flex-col'>
-        <Navbar />
+// export default function Hero() {
+//   return (
+//     <div className='relative overflow-hidden min-h-[550px] sm:min-h-[660px] flex flex-col'>
+//         <Navbar />
 
-        {/* orange circle removed from page-level and will be placed under the person image (inside image wrapper) */}
+//         {/* orange circle removed from page-level and will be placed under the person image (inside image wrapper) */}
 
-        <section
-        className='text-white body-font z-10'>
-            <div className='container mx-auto flex px-5 py-24 md:flex-row flex-col items-center'>
-                <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left
-                mb-16 md:mb-0 items-center text-center relative'>
-                    <h1 className='title-font sm:text-4xl mb-4 font-bold text-white'>HELLO I'm SARUNWIT</h1>
-                    <p className='mb-8 leading-relaxed'>ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+//         <section
+//         className='text-white body-font z-10'>
+//             <div className='container mx-auto flex px-5 py-24 md:flex-row flex-col items-center'>
+//                 <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left
+//                 mb-16 md:mb-0 items-center text-center relative'>
+//                     <h1 className='title-font sm:text-4xl mb-4 font-bold text-white'>HELLO I'm SARUNWIT</h1>
+//                     <p className='mb-8 leading-relaxed'>ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
 
-                    </p>
-                    <div className='flex justify-center'>
-                        <a href={CV} download>
-                            <button className='inline-flex text-white bg-orange-500 border-0 py-2 px-6
-                            focus:outline-none hover:bg-orange-600 hover:shadow-[0_0_40px_rgb(255, 165, 0, 0.7)]
-                            rounded-full text-lg'>
-                                Download CV
-                            </button>
-                        </a>
-                    </div>
-                </div>
-                <div className='lg:max-w-lg lg:w-full md:w-1/2 w-5/6 flex justify-center md:justify-end items-center relative'>
-                    {/* orange circle placed behind and slightly below the photo */}
-                    <div className='absolute right-[-60px] bottom-[-40px] z-0 pointer-events-none hidden md:block'>
-                      <div className='w-[420px] h-[420px] rounded-full bg-gradient-to-r from-orange-600 via-yellow-500 to-red-400 shadow-[0_0_80px_rgba(255,165,0,0.6)]'></div>
-                    </div>
-                    {/* mobile smaller circle */}
-                    <div className='absolute right-[-20px] bottom-[-20px] z-0 pointer-events-none block md:hidden'>
-                      <div className='w-56 h-56 rounded-full bg-gradient-to-r from-orange-600 via-yellow-500 to-red-400 shadow-[0_0_60px_rgba(255,165,0,0.6)]'></div>
-                    </div>
+//                     </p>
+//                     <div className='flex justify-center'>
+//                         <a href={CV} download>
+//                             <button className='inline-flex text-white bg-orange-500 border-0 py-2 px-6
+//                             focus:outline-none hover:bg-orange-600 hover:shadow-[0_0_40px_rgb(255, 165, 0, 0.7)]
+//                             rounded-full text-lg'>
+//                                 Download CV
+//                             </button>
+//                         </a>
+//                     </div>
+//                 </div>
+//                 <div className='lg:max-w-lg lg:w-full md:w-1/2 w-5/6 flex justify-center md:justify-end items-center relative'>
+//                     {/* orange circle placed behind and slightly below the photo */}
+//                     <div className='absolute right-[-60px] bottom-[-40px] z-0 pointer-events-none hidden md:block'>
+//                       <div className='w-[420px] h-[420px] rounded-full bg-gradient-to-r from-orange-600 via-yellow-500 to-red-400 shadow-[0_0_80px_rgba(255,165,0,0.6)]'></div>
+//                     </div>
+//                     {/* mobile smaller circle */}
+//                     <div className='absolute right-[-20px] bottom-[-20px] z-0 pointer-events-none block md:hidden'>
+//                       <div className='w-56 h-56 rounded-full bg-gradient-to-r from-orange-600 via-yellow-500 to-red-400 shadow-[0_0_60px_rgba(255,165,0,0.6)]'></div>
+//                     </div>
 
-                    {/* person image sits above the circle */}
-                    <div className='w-80 h-80 rounded-full overflow-hidden border-4 border-white/10 shadow-[0_10px_30px_rgba(0,0,0,0.6)] relative z-10 bg-gray-900'>
-                      <img src={img} alt="hero" className='w-full h-full object-cover object-top' />
-                    </div>
-                </div>
-            </div>
-        </section>
-    </div>
-  )
-}
+//                     {/* person image sits above the circle */}
+//                     <div className='w-80 h-80 rounded-full overflow-hidden border-4 border-white/10 shadow-[0_10px_30px_rgba(0,0,0,0.6)] relative z-10 bg-gray-900'>
+//                       <img src={img} alt="hero" className='w-full h-full object-cover object-top' />
+//                     </div>
+//                 </div>
+//             </div>
+//         </section>
+//     </div>
+//   )
+// }
