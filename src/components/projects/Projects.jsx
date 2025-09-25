35d2f8053a404d1340405efc7fc88814e48562c6
Sarunwit00 <a.sarunwit@gmail.com>
2025-09-25 17:58:45 +0700

    fix_Save8

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -15,40 +15,44 @@ export default function Projects() {
     const settings ={
         dots: true,
         infinite: true,
-        slidersToShow: 3,
-        slidersToScroll: 1,
+        slidesToShow: 3,
+        slidesToScroll: 1,
         arrows: true,
         responsive: [
             {
                 breakpoint: 700,
                 settings: {
-                    slidersToShow: 1,
+                    slidesToShow: 1,
                 }
             }
         ]
     };
   return (
   <section data-aos='fade-up' data-aos-delay='400' id='projects' className='relative overflow-hidden flex flex-col text-white body-font'>
-        <div className='container px-5 py-24 ma-auto'>
+        <div className='container px-5 py-24 mx-auto'>
             <h2 className='text-4xl font-bold text-center mb-12'>My Projects</h2>
-            <Slider {...settings}>
-                {
-                    ListProjects.map((project)=>{
-                        return(
-                        <div key={project.id} className='p-4' >
-                            <div className='h-full border-2 border-orange-400 shadow-[0_0_40px_rgb(255, 165, 0, 0.7)] border-opacity-60
-                             rounded-lg overflow-hidden'>
-                                <img src={project.image} alt={project.title} className='w-full h-auto lg:h-48 sm:h-24 object-cover object-center' />
-                                <div className='p-6'>
-                                    <h2 className='tracking-widest text-xl title-font font-medium text-gray-400 mb-1'>{project.title}</h2>
-                                    <p className='leading-relaxed mb-3'>{project.description}</p>
+
+            {/* wrapper จำกัดความกว้างและจัดกึ่งกลาง */}
+            <div className='max-w-5xl w-full mx-auto'>
+                <Slider {...settings}>
+                    {
+                        ListProjects.map((project)=>{
+                            return(
+                            <div key={project.id} className='p-4' >
+                                <div className='h-full border-2 border-orange-400 shadow-[0_0_40px_rgb(255, 165, 0, 0.7)] border-opacity-60
+                                 rounded-lg overflow-hidden'>
+                                    <img src={project.image} alt={project.title} className='w-full h-auto lg:h-48 sm:h-24 object-cover object-center' />
+                                    <div className='p-6'>
+                                        <h2 className='tracking-widest text-xl title-font font-medium text-gray-400 mb-1'>{project.title}</h2>
+                                        <p className='leading-relaxed mb-3'>{project.description}</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        )
-                    })
-                }
-            </Slider>
+                            )
+                        })
+                    }
+                </Slider>
+            </div>
         </div>
   </section>
   )
