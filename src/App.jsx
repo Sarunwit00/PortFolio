1a83e3bddf68d0352ab4e0145aace92b0032325a
Sarunwit00 <a.sarunwit@gmail.com>
2025-09-25 19:40:46 +0700

    Revert "....."
    This reverts commit e65407b9ab982ba12186a41420c9461515cff54d.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React, {useEffect} from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
-import Layout from './components/layout/Layout'
 import Hero from './components/hero/Hero'
 import Skills from './components/skills/Skills'
 import Projects from './components/projects/Projects'
-import Experience from './components/experience/Experience'
 import Contact from './components/contact/Contact'
-import SectionTitle from './components/ui/SectionTitle'
+// import Experience from './components/experience/Experience'
 
 export default function App() {
   useEffect(() => {
@@ -16,38 +14,12 @@ export default function App() {
     })
   }, [])
   return (
-    <Layout>
-      <section className="px-4">
-        <Hero />
-      </section>
-
-      <section className="px-4 pt-24">
-        <SectionTitle id="skills">Skills</SectionTitle>
-        <div className="max-w-5xl mx-auto px-4">
-          <Skills />
-        </div>
-      </section>
-
-      <section className="px-4 pt-24">
-        <SectionTitle id="projects">My Projects</SectionTitle>
-        <div className="max-w-6xl mx-auto px-4">
-          <Projects />
-        </div>
-      </section>
-
-      <section className="px-4 pt-24">
-        <SectionTitle id="experience">Experience</SectionTitle>
-        <div className="max-w-4xl mx-auto px-4">
-          <Experience />
-        </div>
-      </section>
-
-      <section className="px-4 pt-24 pb-12">
-        <SectionTitle id="contact">Contact</SectionTitle>
-        <div className="max-w-2xl mx-auto px-4">
-          <Contact />
-        </div>
-      </section>
-    </Layout>
+    <main className=' bg-[#0d182e]'>
+      <Hero />
+      <Skills />
+      {/* <Experience /> */}
+      <Projects />
+      <Contact />
+    </main>
   )
 }
