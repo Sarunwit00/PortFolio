000c1bdde0909baaae2156b341d9b507d89422fd
Sarunwit00 <a.sarunwit@gmail.com>
2025-09-25 18:47:40 +0700

    ...

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,15 +6,15 @@ import img from '../../assets/img.png'
 export default function Hero() {
   return (
     <div className='relative overflow-hidden min-h-[550px] sm:min-h-[660px] flex flex-col'>
-        <div className='md:h-[720px] h-[640px] md:w-[640px] w-[880px] right-0 bg-gradient-to-r
-        from-orange-600 via-yellow-500 to-red-400 absolute rounded-full transform
-        rotate-12 -top-20 shadow-[0_0_40px_rgb(255, 165, 0, 0.7)]'
-        >
-        </div>
         <Navbar />
+
+        {/* orange circle background */}
+        <div className='pointer-events-none absolute right-0 -top-12 md:-top-24 z-0'>
+          <div className='hidden md:block w-[540px] h-[540px] rounded-full bg-gradient-to-r from-orange-600 via-yellow-500 to-red-400 shadow-[0_0_80px_rgba(255,165,0,0.6)] transform translate-x-24'></div>
+          <div className='block md:hidden w-72 h-72 rounded-full bg-gradient-to-r from-orange-600 via-yellow-500 to-red-400 shadow-[0_0_60px_rgba(255,165,0,0.6)]'></div>
+        </div>
+
         <section
-        data-aos='fade-up'
-        data-aos-delay='250'
         className='text-white body-font z-10'>
             <div className='container mx-auto flex px-5 py-24 md:flex-row flex-col items-center'>
                 <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left
